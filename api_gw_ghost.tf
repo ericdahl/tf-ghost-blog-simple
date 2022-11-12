a25c3aef83a9437ad602895cdd780c43bdae29af
Eric Dahl <eric.c.dahl@gmail.com>
2022-11-12 12:55:42 -0800

    enable access logs

--- api_gw_ghost.tf
+++ api_gw_ghost.tf
@@ -40,10 +40,23 @@ resource "aws_apigatewayv2_stage" "ghost" {
   api_id      = aws_apigatewayv2_api.ghost.id
   name        = "$default"
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_ghost.arn
+    format          = <<EOF
+$context.identity.sourceIp - - [$context.requestTime] "$context.httpMethod $context.routeKey $context.protocol" $context.status $context.responseLength $context.requestId
+EOF
+  }
 }
 
 resource "aws_apigatewayv2_vpc_link" "ghost" {
   name               = "ghost"
   security_group_ids = [aws_security_group.ghost_api_gw_vpc_link.id]
   subnet_ids         = [for s in aws_subnet.public : s.id]
+}
+
+resource "aws_cloudwatch_log_group" "api_gw_ghost" {
+  name = "apigw-ghost"
+
+  retention_in_days = 1
 }
